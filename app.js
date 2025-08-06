1f4e3673705e691ea02c41321f7450f79ea6b1fa
LaxmiPkul <thandaroopkul@gmail.com>
2025-08-06 12:09:05 +0700

    added

---
+++ app.js
@@ -0,0 +1,21 @@
+// let btns = document.querySelectorAll('.btns');
+
+// btns.forEach(function(btn) {
+//   btn.addEventListener('click', function() {
+//     console.log(btn.textContent);
+//   });
+// });
+
+let add = document.querySelector('.add');
+let sub = document.querySelector('.sub');
+let  mul= document.querySelector('.multi');
+let div = document.querySelector('.div');
+
+let result = document.querySelector('.result');
+let num1 = document.querySelector('.input1');
+let num2 = document.querySelector('.input2');
+let h1 = document.querySelector('h1');
+
+num1.addEventListener('input', function() {
+  h1.textContent = `Result: ${num1.value}`;
+});
