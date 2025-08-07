f837cf99f83bc303c8110f0b326f3499db5545e5
LaxmiPkul <thandaroopkul@gmail.com>
2025-08-07 18:36:15 +0700

    changes

--- app.js
+++ app.js
@@ -6,16 +6,17 @@
 //   });
 // });
 
-let add = document.querySelector('.add');
-let sub = document.querySelector('.sub');
-let  mul= document.querySelector('.multi');
-let div = document.querySelector('.div');
+// let add = document.querySelector('.add');
+// let sub = document.querySelector('.sub');
+// let  mul= document.querySelector('.multi');
+// let div = document.querySelector('.div');
 
-let result = document.querySelector('.result');
-let num1 = document.querySelector('.input1');
-let num2 = document.querySelector('.input2');
-let h1 = document.querySelector('h1');
+// let result = document.querySelector('.result');
+// let num1 = document.querySelector('.input1');
+// let num2 = document.querySelector('.input2');
+// let h1 = document.querySelector('h1');
 
-num1.addEventListener('input', function() {
-  h1.textContent = `Result: ${num1.value}`;
-});
+// num1.addEventListener('input', function() {
+//   h1.textContent = `Result: ${num1.value}`;
+// });
+
